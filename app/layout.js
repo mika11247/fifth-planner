5a81dbae5b87b457a9e8a59284b0e3855a6a6669
Mika <mika11247@gmail.com>
2026-05-14 17:58:16 +0900

    ファビコン

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,13 @@ import { AppShell } from "@/components/AppShell";
 export const metadata = {
   title: "Fifth planner",
   description: "人生を俯瞰するデジタルライフプランナー",
+
   icons: {
-    icon: "/favicon.svg",
-    apple: "/apple-icon.svg"
-  }
+    icon: "/icon-192.png",
+    apple: "/apple-touch-icon.png",
+  },
+
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
